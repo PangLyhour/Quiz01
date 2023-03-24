4480eec82b2b2ae4fcd2cb14b20fc454946375a4
Saingnam <126489617+Saingnam@users.noreply.github.com>
2023-03-24 21:58:32 +0700

    Add files via upload

---
+++ ResultList.java
@@ -0,0 +1,18 @@
+package Quiz01;
+
+public class ResultList {
+    public static void main(String[] args) {
+        System.out.println(
+            """
+            Test Result:
+                Category                Score
+                WPM                     82
+                acc                     89%
+                raw                     59
+                characters              89//5/6/0
+                consistency             76%
+                time                    45s
+            """
+        );
+    }
+}
