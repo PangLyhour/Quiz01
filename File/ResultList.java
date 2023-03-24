708cec703e5b98e507db618e27661461949638a9
PangLyhour <128688193+PangLyhour@users.noreply.github.com>
2023-03-24 16:31:50 +0700

    Add files via upload

---
+++ File/ResultList.java
@@ -0,0 +1,24 @@
+package File;
+
+public class ResultList {
+    void Display(){
+        System.out.println(
+            """
+            Test Result:
+                Subject                 Mark
+                OOP                     70
+                IPE                     71
+                Database                68
+                Automata                56
+                Web Design              94
+                Matlab                  40
+                Theory of Information   67
+        ******************************************
+                Average                 68
+            """
+        );
+    }
+
+    public static void main(String[] args) {
+    }
+}
